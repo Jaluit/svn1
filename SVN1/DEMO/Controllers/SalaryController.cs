19c1cbc92cb40f1b4b77cf467028977f0de02717
JALUIT <Jaluit730@hotmail.com>
2020-02-16 16:24:28 +0800

    建立專案

---
+++ SVN1/DEMO/Controllers/SalaryController.cs
@@ -0,0 +1,19 @@
+﻿using DEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DEMO.Controllers
+{
+    public class SalaryController : ApiController
+    {
+ 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ SVN1/DEMO/Models/SalaryViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEMO.Models
+{
+    public class SalaryViewModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public short LeaveHours { get; set; }
+        public short OverHours { get; set; }
+        public int BasicPrice { get; set; }
+        public int Price { get; set; }
+    }
+}
